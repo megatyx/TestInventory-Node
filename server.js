bfdf765d538eeb7857bb249a8156d8fe38e6cf38
Wells, Tyler | Tyler | PAYSD <tyler.wells@rakuten.com>
2016-09-26 16:01:55 +0900

    make it better

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware = require('./Model/middleware.js');
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(request, response){
+
+	response.send('About us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+
+	console.log('Express server started on port: ' + PORT + '!');
+});
