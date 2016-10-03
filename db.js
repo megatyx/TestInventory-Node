8e97144eb73b66e21a243fdb0cfd3b897580da3e
Wells, Tyler | Tyler | PAYSD <tyler.wells@rakuten.com>
2016-10-03 14:05:59 +0900

    debug

--- db.js
+++ db.js
@@ -3,15 +3,15 @@ var sequelize;
 
 //if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      logging:  false
-    });
+    // sequelize = new Sequelize(process.env.DATABASE_URL, {
+    //   logging:  false
+    // });
 //}
 // else {
-// 	sequelize = new Sequelize(undefined, undefined, undefined, {
-// 		dialect: 'sqlite',
-// 		storage: __dirname + '/data/dev-todo-api.sqlite'
-// 	});
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		dialect: 'sqlite',
+		storage: __dirname + '/data/dev-todo-api.sqlite'
+	});
 // }
 
 var db = {};
