48654a7985d3c65e0a4f9419962ea020539b3602
Wells, Tyler | Tyler | PAYSD <tyler.wells@rakuten.com>
2016-10-03 13:01:32 +0900

    fixed postgres call

--- db.js
+++ db.js
@@ -5,14 +5,11 @@ var sequelize;
 if(env === 'production')
 {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-
 		dialect: 'postgres'
 	});
 
-}
-else
-{
-	var sequelize = new Sequelize(undefined, undefined, undefined, {
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
 		dialect: 'sqlite',
 		storage: __dirname + '/data/dev-todo-api.sqlite'
 	});
