93d40f62bffe4c58a29b4640b1c0c26abcf38158
Wells, Tyler | Tyler | PAYSD <tyler.wells@rakuten.com>
2016-10-03 13:47:16 +0900

    postgres fix 5

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 var Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-//if(env === 'production')
-//{
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: 'postgres'
+if(process.env.HEROKU_POSTGRESQL_BRONZE_URL)
+{
+	sequelize = new Sequelize(HEROKU_POSTGRESQL_BRONZE_URL, {
+		dialect: 'postgres',
+		protocol: 'postgres'
 	});
 
-//} 
-// else {
-// 	sequelize = new Sequelize(undefined, undefined, undefined, {
-// 		dialect: 'sqlite',
-// 		storage: __dirname + '/data/dev-todo-api.sqlite'
-// 	});
-// }
+} 
+else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		dialect: 'sqlite',
+		storage: __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
 
 var db = {};
 
