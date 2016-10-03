915b83ac9f284052b3ec5758dbf494f19c883b3e
Wells, Tyler | Tyler | PAYSD <tyler.wells@rakuten.com>
2016-10-03 13:53:37 +0900

    postgres fix 6

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 var Sequelize = require('sequelize');
 var sequelize;
 
-if(process.env.HEROKU_POSTGRESQL_BRONZE_URL)
-{
-	sequelize = new Sequelize(HEROKU_POSTGRESQL_BRONZE_URL, {
-		dialect: 'postgres',
-		protocol: 'postgres'
-	});
-
-} 
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    });
+}
 else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		dialect: 'sqlite',
